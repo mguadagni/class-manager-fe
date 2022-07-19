dcfdfce67aa7e500bad7ba26641a73e470c9b05e
mguadagni <guadagnimichael@gmail.com>
2022-07-18 21:22:34 -0400

    Testing/Learning React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test/Test';
+import Foo from './components/Test/Foo';
 
 function App() {
   return (
     <div className="App">
       <Test />
+      <Foo />
     </div>
   );
 }
--- src/components/Test/Foo.js
+++ src/components/Test/Foo.js
@@ -1,2 +1,24 @@
 //make another componenet and play with it. use state and button to do something
 
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Foo = () => {
+
+    useEffect(() => {
+        const getSchools = async() => {
+            const res = await axios.get("http://localhost:8080/api/schools/");
+            console.log(res.data[1]);
+        }
+        getSchools();
+    }, [])
+
+    return (
+        <div>
+            <p>Test Foo File</p>
+            {/* <p>{res[0]}</p> */}
+        </div>
+    )
+}
+
+export default Foo;
